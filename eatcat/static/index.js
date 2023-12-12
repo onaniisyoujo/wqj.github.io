92556047ddd5d1263901613caf79fb430d7aaf1a
onaniisyoujo <crazycute@163.com>
2023-12-12 21:50:51 +0800

    更新 index.js

--- eatcat/static/index.js
+++ eatcat/static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false;
 let _fsj = false;
-var url = 'https://onaniisyoujo.github.io/eatcat/static/image/ClickBefore.png';
+var url = 'https://onaniisyoujo.github.io/wqj.github.io/eatcat/static/image/ClickBefore.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
